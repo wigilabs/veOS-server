d974319a53dd48370cdc1c84482f4f8e247e7194
Dieter Evangelista <hansevangelista@gmail.com>
2020-04-01 12:27:12 -0500

    ONOFF queries

--- pages/api/unique-machines.js
+++ pages/api/unique-machines.js
@@ -6,7 +6,7 @@ const cors = Cors()
 const handler = (req, res) => {
 
   const URL = 'https://insights-api.newrelic.com/v1/accounts/2673149/query?nrql=SELECT%20uniques(machine_id)%20FROM%20Heartbeat'
-  const AuthStr = 'NRIQ-dBGqek4MzSXRGUO6T620tBT50oe14fXI';
+  const AuthStr = 'NRIQ-dBGqek4MzSXRGUO6T620tBT50oe14fXI'
 
   axios.get(URL, { headers: { "X-Query-Key": AuthStr } })
  .then(response => {
